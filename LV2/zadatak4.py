1a1ab4eac21fbd3b29f6906ede38419c60a92bbc
Dominik Župarić <107096417+DZuparic@users.noreply.github.com>
2023-04-05 10:17:39 +0200

    Update zadatak4.py

--- LV2/zadatak4.py
+++ LV2/zadatak4.py
@@ -1,7 +1,17 @@
-import numpy as np
 import matplotlib.pyplot as plt
-check = np.zeros((5, 5))
-check[::2, 1::2] = 1
-check[1::2, ::2] = 1
-plt.matshow(check, cmap='gray')
+import numpy as np
+
+def build_checkerboard(v, a, b):
+    x = np.zeros((v,v))
+    y = np.ones((v,v))
+    r1 = (np.hstack((x,y)))
+    r1 = r1.copy()*(b/50)
+    r2 = (np.hstack((y,x)))*b
+    r2 = r2.copy()*(b/50)
+    check=(np.vstack((r1,r2)))*a
+    return check
+
+ploca = build_checkerboard(50,200,300)
+
+plt.imshow(ploca, cmap='gray', vmin=0, vmax=255)
 plt.show()
